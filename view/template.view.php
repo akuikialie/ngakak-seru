3246b5157dec307417d7b4a123d5160575b59371
AkhyarMaulana <akhyarmaulana99@gmail.com>
2014-11-29 20:14:51 +0700

    Membuat logo Ngakak Seru , Kordinat Map, Kolom konten dan iklan

--- view/template.view.php
+++ view/template.view.php
@@ -119,11 +119,11 @@
 			$('#googlemaps').gMap({
 				maptype: 'ROADMAP',
 				scrollwheel: false,
-				zoom: 13,
+				zoom: 15,
 				markers: [
 					{
-						address: 'Los Angeles, United States', // Your Adress Here
-						html: '',
+						address: '-6.331917, 106.960581', // Your Adress Here
+						html: 'DDAcademy Ngakak Seru',
 						popup: false,
 					}
 
@@ -143,135 +143,29 @@
 			
 			<!-- start: Row -->
 			<div class="row">
-			
-				<!-- start: Contact Info -->
-				<div class="span3">
-					<div class="title"><h3>Contact Info</h3></div>
-					<p>
-						<b>Company Name</b>
+                <div class="span8">
+                <h2>Content And Image Here</h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
 					</p>
-					<p>
-						30 South Park Avenue
-					</p>
-					<p>	
-						San Francisco, CA 94108, USA
-					</p>	
-					<p>	
-						Phone: (123) 456-7890
-					</p>
-					<p>	
-						Fax: +08 (123) 456-7890
-					</p>
-					<p>
-						Email: contact@companyname.com
-					</p>
-					<p>
-						Web: companyname.com
-					</p>
-				</div>
-				<!-- end: Contact Info -->		
+                </div>
+                <div class="span4">
+                <h2>Ads Here</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                </p>
+                </div>
 
-				<!-- start: Contact Form -->
-				<div class="span4">
-					<div class="title"><h3>Contact Form</h3></div>
-
-					<!-- start: Contact Form -->
-					<div id="contact-form">
-
-						<form method="post" action="">
-
-							<fieldset>
-								<div class="clearfix">
-									<label for="name"><span>Name:</span></label>
-									<div class="input">
-										<input tabindex="1" size="18" id="name" name="name" type="text" value="">
-									</div>
-								</div>
-
-								<div class="clearfix">
-									<label for="email"><span>Email:</span></label>
-									<div class="input">
-										<input tabindex="2" size="25" id="email" name="email" type="text" value="" class="input-xlarge">
-									</div>
-								</div>
-
-								<div class="clearfix">
-									<label for="message"><span>Message:</span></label>
-									<div class="input">
-										<textarea tabindex="3" class="input-xlarge" id="message" name="body" rows="7"></textarea>
-									</div>
-								</div>
-
-								<div class="actions">
-									<button tabindex="3" type="submit" class="btn btn-succes btn-large">Send message</button>
-								</div>
-							</fieldset>
-
-						</form>
-
-					</div>
-					<!-- end: Contact Form -->
-
-				</div>
-				<!-- end: Contact Form -->
-
-				<!-- start: Social Sites -->
-				<div class="span5">
-					<div class="title"><h3>Follow US!</h3></div>
-					<ul class="social-bookmarks">
-						<li class="aim"><a href="#">aim</a></li>
-						<li class="apple"><a href="#">apple</a></li>
-						<li class="behance"><a href="#">behance</a></li>
-						<li class="blogger"><a href="#">blogger</a></li>
-						<li class="cargo"><a href="#">cargo</a></li>
-						<li class="delicious"><a href="#">delicious</a></li>
-						<li class="deviantart"><a href="#">deviantart</a></li>
-						<li class="digg"><a href="#">digg</a></li>
-						<li class="dopplr"><a href="#">dopplr</a></li>
-						<li class="dribbble"><a href="#">dribbble</a></li>
-						<li class="ember"><a href="#">ember</a></li>
-						<li class="evernote"><a href="#">evernote</a></li>
-						<li class="facebook"><a href="https://www.facebook.com/brankic1979themes">facebook</a></li>
-						<li class="flickr"><a href="http://www.flickr.com/photos/brankic1979/">flickr</a></li>
-						<li class="forrst"><a href="#">forrst</a></li>
-						<li class="github"><a href="#">github</a></li>
-						<li class="google"><a href="#">google</a></li>
-						<li class="googleplus"><a href="#">googleplus</a></li>
-						<li class="gowalla"><a href="#">gowalla</a></li>
-						<li class="grooveshark"><a href="#">grooveshark</a></li>
-						<li class="html5"><a href="#">html5</a></li>
-						<li class="icloud"><a href="#">icloud</a></li>
-						<li class="lastfm"><a href="#">lastfm</a></li>
-						<li class="linkedin"><a href="#">linkedin</a></li>
-						<li class="metacafe"><a href="#">metacafe</a></li>
-						<li class="mixx"><a href="#">mixx</a></li>
-						<li class="myspace"><a href="#">myspace</a></li>
-						<li class="netvibes"><a href="#">netvibes</a></li>
-						<li class="newsvine"><a href="#">newsvine</a></li>
-						<li class="orkut"><a href="#">orkut</a></li>
-						<li class="paypal"><a href="#">paypal</a></li>
-						<li class="picasa"><a href="#">picasa</a></li>
-						<li class="pinterest"><a href="#">pinterest</a></li>
-						<li class="plurk"><a href="#">plurk</a></li>
-						<li class="posterous"><a href="#">posterous</a></li>
-						<li class="reddit"><a href="#">reddit</a></li>
-						<li class="rss"><a href="#">rss</a></li>
-						<li class="skype"><a href="#">skype</a></li>
-						<li class="stumbleupon"><a href="#">stumbleupon</a></li>
-						<li class="technorati"><a href="#">technorati</a></li>
-						<li class="tumblr"><a href="#">tumblr</a></li>
-						<li class="twitter"><a href="#">twitter</a></li>
-						<li class="vimeo"><a href="#">vimeo</a></li>
-						<li class="wordpress"><a href="#">wordpress</a></li>
-						<li class="yahoo"><a href="#">yahoo</a></li>
-						<li class="yelp"><a href="#">yelp</a></li>
-						<li class="youtube"><a href="#">youtube</a></li>
-						<li class="zerply"><a href="#">zerply</a></li>
-						<li class="zootool"><a href="#">zootool</a></li>
-					</ul>
-				</div>
-				<!-- end: Social Sites -->
-			
 			</div>
 			<!-- end: Row -->
 
@@ -364,7 +258,7 @@
 				<!-- start: Photo Stream -->
 				<div class="span3">
 
-					<h3>Photo Stream</h3>
+					<h3>Team's Photo</h3>
 					<div class="flickr-widget">
 							<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=9&display=latest&size=s&layout=x&source=user&user=29609591@N08"></script>
 						<div class="clear"></div>
@@ -465,7 +359,7 @@
 		<div class="container">
 		
 			<p>
-				&copy; 2013, creativeLabs. <a href="http://bootstrapmaster.com" alt="Bootstrap Themes">Bootstrap Themes</a> designed by BootstrapMaster in Poland <img src="<?=base_url();?>../img/poland2.png" alt="Poland" style="margin-top:-4px">
+				&copy; 2014, DDAcademy. <a href="http://ngakakseru.com" alt="PT. Ngakak Seru Indonesia">PT Ngakak Seru Indonesia</a> <br>designed by PangeranWeb in Indonesia <img src="<?=base_url();?>../img/poland2.png" alt="Poland" style="margin-top:-4px">
 			</p>
 	
 		</div>
