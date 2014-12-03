fb6592363ed57d8d0c97592b91dd3ec5c475efe2
Muh Shalahuddin <dda.shalahuddin@gmail.com>
2014-12-03 13:38:40 +0700

    slicing header, head, sidebar dari index. membuat file PDO

---
+++ view/head/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>NgakakSeru- Santai Aja Kali</title> 
+	<meta name="description" content="Ngakak Seru Website Lucu"/>
+	<meta name="keywords" content="fun,funny,lol,meme,GIF,wtf,omg,fail,video,cosplay,geeky,forever alone" />
+	<meta name="author" content="DDA Academy"/>
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: Facebook Open Graph -->
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content=""/>
+	<meta property="og:type" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<!-- end: Facebook Open Graph -->
+
+    <!-- start: CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">
+    <link href="../css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+	<!-- end: CSS -->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
--- view/template.view.php
+++ view/template.view.php
@@ -1,109 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-	<!-- start: Meta -->
-	<meta charset="utf-8">
-	<title>NgakakSeru- Santai Aja Kali</title> 
-	<meta name="description" content="Ngakak Seru Website Lucu"/>
-	<meta name="keywords" content="fun,funny,lol,meme,GIF,wtf,omg,fail,video,cosplay,geeky,forever alone" />
-	<meta name="author" content="DDA Academy"/>
-	<!-- end: Meta -->
-	
-	<!-- start: Mobile Specific -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<!-- end: Mobile Specific -->
-	
-	<!-- start: Facebook Open Graph -->
-	<meta property="og:title" content=""/>
-	<meta property="og:description" content=""/>
-	<meta property="og:type" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:image" content=""/>
-	<!-- end: Facebook Open Graph -->
-
-    <!-- start: CSS -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
-    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="../css/style.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
-	<!-- end: CSS -->
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
+<?php include ('head/head.php'); ?> 
 </head>
 <body>
 	
 	<!--start: Header -->
-	<header>
-		
-		<!--start: Container -->
-		<div class="container">
-			
-			<!--start: Row -->
-			<div class="row">
-					
-				<!--start: Logo -->
-				<div class="logo span3">
-						
-					<a class="brand" href="#"><img src="<?=base_url();?>../img/logo.png"></a>
-						
-				</div>
-				<!--end: Logo -->
-					
-				<!--start: Navigation -->
-				<div class="span9">
-						
-					<div class="navbar navbar-inverse">
-			    		<div class="navbar-inner">
-			        		<div class="container">
-			          			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-			            			<span class="icon-bar"></span>
-			            			<span class="icon-bar"></span>
-			            			<span class="icon-bar"></span>
-			          			</a>
-			          			<div class="nav-collapse collapse">
-			            			<ul class="nav">
-			              				<li><a href="index.html">TOP</a></li>
-			              				<li><a href="about.html">Trending</a></li>
-										<li><a href="services.html">Terbaru</a></li>
-										<li><a href="pricing.html">TV</a></li>
-			              				<li class="dropdown">
-			                				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Kategori <b class="caret"></b></a>
-			                				<ul class="dropdown-menu">
-			                  					<li><a href="#">Cewe</a></li>
-			                  					<li><a href="#">NSWF</a></li>
-			                  					<li><a href="#">GIF</a></li>
-			                  					<li><a href="#">Meme</a></li>
-			                  					<li><a href="#">Imut</a></li>
-			                  					<li><a href="#">Cosplay</a></li>
-			                  					<li><a href="#">Timely</a></li>
-			                  					<li><a href="#">Komik</a></li>
-			                  					<li><a href="#">Makanan</a></li>
-			                				</ul>
-			              				</li>
-			            			</ul>
-			          			</div>
-			        		</div>
-			      		</div>
-			    	</div>
-					
-				</div>	
-				<!--end: Navigation -->
-					
-			</div>
-			<!--end: Row -->
-			
-		</div>
-		<!--end: Container-->			
-			
+	<header>	
+	<?php include('head/header.php');?>
 	</header>
 	<!--end: Header-->
 
@@ -198,29 +102,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="span4">
-                    <div class="img-container-ads">
-                        <img src="../img/bg-grey.png">
-                    </div>
-                    <div class="img-container-ads">
-                        <img src="../img/bg-grey.png">
-                    </div>
-                    <div class="img-container-ads">
-                        <img src="../img/bg-grey.png">
-                    </div>
-                    <h2>Subscribe</h2>
-                        <input type="text" name="subcribe" class="form-control">
-                        <button class="btn btn-default btn-md">Subscribe</button>
-                        <br><br>
-                    <h2>Get The App</h2>
-                        <button class="btn btn-default">DOWNLOAD</button>
-                        <br><br>
-                    <div id="footer">
-                        <a href="">Pasang Iklan</a> · <a href="">Kontak</a> · <a href="">Privasi</a> · <a href="">Peraturan</a>
-                        <br>
-                        www.NgakakSeru.com © 2014</a>
-                    </div>
-                </div>
+                <?php include ('sidebar/sidebar.php'); ?>
 
 			</div>
 			<!-- end: Row -->
