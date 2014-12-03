ddad435d5291637b96f46284fb1b0ca25349b9b8
Muh Shalahuddin <dda.shalahuddin@gmail.com>
2014-12-03 17:28:58 +0700

    merubah file PDO ke dalam bentuk class

--- view/scripts/db.php
+++ view/scripts/db.php
@@ -3,6 +3,14 @@
 //DBusername - your mysql username
 //DBpassword - your mysql password
 //DBname - your mysql database name
+class DatabaseConnectionManager
+{
+	public function createConnection()
+	{
 		$db = new PDO('mysql:host=localhost;dbname=ngakakseru', 'root', 'master753');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		return $db;
+	}
+}
 ?>
