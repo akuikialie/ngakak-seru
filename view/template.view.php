bc04871904e103920105fc134ed1cb191d9248f5
ali@firzil.co.id <achmadali1992@yahoo.com>
2014-12-06 13:58:08 +0700

    Fix bug in homepage

--- view/template.view.php
+++ view/template.view.php
@@ -1,103 +1,103 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<?php include ('head/head.php'); ?> 
-</head>
-<body>
-	
-	<!--start: Header -->
-	<header>	
+    <head>
+        <?php include ('head/head.php'); ?> 
+    </head>
+    <body>
+
+        <!--start: Header -->
+        <header>	
 	<?php include('head/header.php');?>
-	</header>
-	<!--end: Header-->
+        </header>
+        <!--end: Header-->
 
-	<!-- start: Wrapper -->	
-	<div id="wrapper">		
+        <!-- start: Wrapper -->	
+        <div id="wrapper">		
 
-		<!-- start: Container -->	
-		<div class="container">
-			
-			<!-- start: Row -->
-			<div class="row">
-                <div class="span8 main-container">
-                    <div class="content-container">
-                        <div class="post-container">
-                            <h2>Judul Konten</h2><br>
-                            <div class="img-container">
-                                <img src="../main/img/default-image.png">
+            <!-- start: Container -->	
+            <div class="container">
+
+                <!-- start: Row -->
+                <div class="row">
+                    <div class="span8 main-container">
+                        <div class="content-container">
+                            <div class="post-container">
+                                <h2>Judul Konten</h2><br>
+                                <div class="img-container">
+                                    <img src="../main/img/default-image.png">
+                                </div>
+                            </div>
+                            <div class="post-tool">
+                                <ul class="social-bookmarks ">
+                                    <li class="facebook" title="Share"><a href="#">facebook</a></li>
+                                    <li class="twitter" title="Tweet"><a href="#">twitter</a></li>
+                                </ul>
+                                <a href="" class="like" title="Suka"><i class="ico-thumbs-up  ico-white circle-color-full small-color"></i></a>
+                                <a href="" class="like" title="Tidak Suka"><i class="ico-thumbs-down  ico-white circle-color-full small-color"></i></a>
+                                <a href="" class="like" title="Komentar"><i class="ico-chat ico-white circle-color-full small-color"></i></a>
                             </div>
                         </div>
-                        <div class="post-tool">
-                            <ul class="social-bookmarks ">
-                                <li class="facebook" title="Share"><a href="#">facebook</a></li>
-                                <li class="twitter" title="Tweet"><a href="#">twitter</a></li>
-                            </ul>
-                            <a href="" class="like" title="Suka"><i class="ico-thumbs-up  ico-white circle-color-full small-color"></i></a>
-                            <a href="" class="like" title="Tidak Suka"><i class="ico-thumbs-down  ico-white circle-color-full small-color"></i></a>
-                            <a href="" class="like" title="Komentar"><i class="ico-chat ico-white circle-color-full small-color"></i></a>
-                        </div>
-                    </div>
-                    <div class="content-container">
-                        <div class="post-container">
-                            <h2>Judul Konten</h2><br>
-                            <div class="img-container">
-                                <img src="../main/img/default-image.png">
+                        <div class="content-container">
+                            <div class="post-container">
+                                <h2>Judul Konten</h2><br>
+                                <div class="img-container">
+                                    <img src="../main/img/default-image.png">
+                                </div>
+                            </div>
+                            <div class="post-tool">
+                                <ul class="social-bookmarks ">
+                                    <li class="facebook" title="Share"><a href="#">facebook</a></li>
+                                    <li class="twitter" title="Tweet"><a href="#">twitter</a></li>
+                                </ul>
+                                <a href="" class="like" title="Suka"><i class="ico-thumbs-up  ico-white circle-color-full small-color"></i></a>
+                                <a href="" class="like" title="Tidak Suka"><i class="ico-thumbs-down  ico-white circle-color-full small-color"></i></a>
+                                <a href="" class="like" title="Komentar"><i class="ico-chat ico-white circle-color-full small-color"></i></a>
                             </div>
                         </div>
-                        <div class="post-tool">
-                            <ul class="social-bookmarks ">
-                                <li class="facebook" title="Share"><a href="#">facebook</a></li>
-                                <li class="twitter" title="Tweet"><a href="#">twitter</a></li>
-                            </ul>
-                            <a href="" class="like" title="Suka"><i class="ico-thumbs-up  ico-white circle-color-full small-color"></i></a>
-                            <a href="" class="like" title="Tidak Suka"><i class="ico-thumbs-down  ico-white circle-color-full small-color"></i></a>
-                            <a href="" class="like" title="Komentar"><i class="ico-chat ico-white circle-color-full small-color"></i></a>
-                        </div>
-                    </div>
-                    <div class="content-container">
-                        <div class="post-container">
-                            <h2>Judul Konten</h2><br>
-                            <div class="img-container">
-                                <img src="../main/img/default-image.png">
+                        <div class="content-container">
+                            <div class="post-container">
+                                <h2>Judul Konten</h2><br>
+                                <div class="img-container">
+                                    <img src="../main/img/default-image.png">
+                                </div>
+                            </div>
+                            <div class="post-tool">
+                                <ul class="social-bookmarks ">
+                                    <li class="facebook" title="Share"><a href="#">facebook</a></li>
+                                    <li class="twitter" title="Tweet"><a href="#">twitter</a></li>
+                                </ul>
+                                <a href="" class="like" title="Suka"><i class="ico-thumbs-up  ico-white circle-color-full small-color"></i></a>
+                                <a href="" class="like" title="Tidak Suka"><i class="ico-thumbs-down  ico-white circle-color-full small-color"></i></a>
+                                <a href="" class="like" title="Komentar"><i class="ico-chat ico-white circle-color-full small-color"></i></a>
                             </div>
                         </div>
-                        <div class="post-tool">
-                            <ul class="social-bookmarks ">
-                                <li class="facebook" title="Share"><a href="#">facebook</a></li>
-                                <li class="twitter" title="Tweet"><a href="#">twitter</a></li>
-                            </ul>
-                            <a href="" class="like" title="Suka"><i class="ico-thumbs-up  ico-white circle-color-full small-color"></i></a>
-                            <a href="" class="like" title="Tidak Suka"><i class="ico-thumbs-down  ico-white circle-color-full small-color"></i></a>
-                            <a href="" class="like" title="Komentar"><i class="ico-chat ico-white circle-color-full small-color"></i></a>
-                        </div>
                     </div>
+                    <?php include ('sidebar/sidebar.php'); ?>
+
                 </div>
-                <?php include ('sidebar/sidebar.php'); ?>
+                <!-- end: Row -->
 
-			</div>
-			<!-- end: Row -->
+            </div>
+            <!-- end: Container -->
 
-		</div>
-		<!-- end: Container -->
-				
-  	</div>
-	<!-- end: Wrapper  -->			
-	</div>
+        </div>
+        <!-- end: Wrapper  -->			
+    </div>
     <!-- start: Footer Menu -->
-	
 
-	
 
-<!-- start: Java Script -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="../main/js/jquery-1.8.2.js"></script>
-<script src="../mainmain//js/bootstrap.js"></script>
-<script src="../main/js/flexslider.js"></script>
-<script src="../main/js/carousel.js"></script>
-<script src="../main/js/jquery.cslider.js"></script>
-<script src="../main/js/slider.js"></script>
-<script def src="../main/js/custom.js"></script>
-<!-- end: Java Script -->
+
+
+    <!-- start: Java Script -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../main/js/jquery-1.8.2.js"></script>
+    <script src="../mainmain//js/bootstrap.js"></script>
+    <script src="../main/js/flexslider.js"></script>
+    <script src="../main/js/carousel.js"></script>
+    <script src="../main/js/jquery.cslider.js"></script>
+    <script src="../main/js/slider.js"></script>
+    <script def src="../main/js/custom.js"></script>
+    <!-- end: Java Script -->
 
 </body>
 </html>
